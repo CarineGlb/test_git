18f0ea86b3faee985c986a57fff602ecf16b7baa
Carine Glb <carine_gosalebon@yahoo.fr>
2018-10-26 10:26:49 +0200

    sauvegarde1

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<meta charset ="utf-8"/>
+
+<head>
+
+	<title> Test git</title>
+
+</head>
+<body>
+
+	<h1> Bonjour </h1>
+	
+	<p>bbpwxpcj<ôjs^fj^q</p>
+
+</body>
+</html>
